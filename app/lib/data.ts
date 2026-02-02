60682ad841e95c66614a45c38b0171a76659944c
wafy <zyalwfie@gmail.com>
2026-02-02 19:38:26 +0800

    feat: complete implement chapter 8 - 13
    Chapter 8
    Static and Dynamic Rendering
    
    Chapter 9
    Streaming
    
    Chapter 10
    Adding Search and Pagination
    
    Chapter 11
    Mutating Data
    
    Chapter 12
    Handling Errors
    
    Chapter 13
    Improving Accessibility

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,13 +15,7 @@ export async function fetchRevenue() {
 	try {
 		// Artificially delay a response for demo purposes.
 		// Don't do this in production :)
-
-		// console.log('Fetching revenue data...');
-		// await new Promise((resolve) => setTimeout(resolve, 3000));
-
 		const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-		// console.log('Data fetch completed after 3 seconds.');
 
 		return data;
 	} catch (error) {
@@ -160,6 +154,7 @@ export async function fetchInvoiceById(id: string) {
 			amount: invoice.amount / 100,
 		}));
 
+		console.log(invoice);
 		return invoice[0];
 	} catch (error) {
 		console.error('Database Error:', error);
